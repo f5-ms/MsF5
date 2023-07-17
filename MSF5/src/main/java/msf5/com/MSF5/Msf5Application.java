9baed7b6de4c31728be384c7d34fd2d499eabb01
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2023-07-17 22:21:35 +0200

    Create develop branch

---
+++ MSF5/src/main/java/msf5/com/MSF5/Msf5Application.java
@@ -0,0 +1,13 @@
+package msf5.com.MSF5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msf5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Msf5Application.class, args);
+	}
+
+}
