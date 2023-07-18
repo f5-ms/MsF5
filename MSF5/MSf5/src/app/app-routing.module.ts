0a8aeca58bde47a5430e51af77895a8ecda14730
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2023-07-18 06:15:46 +0200

    add funcion to button ingrese

--- MSF5/MSf5/src/app/app-routing.module.ts
+++ MSF5/MSf5/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path:'',
     component:HeaderComponent
+  },
+  {
+    path: '/dashboard',
+    component: DashboardComponent
   }
 ];
 
