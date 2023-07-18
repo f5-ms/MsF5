480d8cd32459a9894d8820dc5cb18afe87c77006
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2023-07-18 04:46:08 +0200

    finish view to home and editing components

--- MSF5/MSf5/src/app/components/data-form/data-form.component.ts
+++ MSF5/MSf5/src/app/components/data-form/data-form.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-data-form',
   templateUrl: './data-form.component.html',
-  styleUrls: ['./data-form.component.css']
+  styleUrls: ['./data-form.component.css'],
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, NgIf],
 })
 export class DataFormComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+}
 }
--- MSF5/MSf5/src/app/shared/material/material.module.ts
+++ MSF5/MSf5/src/app/shared/material/material.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  exports: [
+  MatToolbarModule,
+  MatListModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
   ]
 })
 export class MaterialModule { }
